302c4b6e30242d028c1dea4bfd534681cb1b8d90
Allen Hsu <allenhsu.taiwan@gmail.com>
2018-12-15 17:19:40 +0800

    fix actionCreator

--- client/src/pages/auth/store/actionCreators.js
+++ client/src/pages/auth/store/actionCreators.js
@@ -3,12 +3,6 @@ import { fromJS } from "immutable";
 import axios from "axios";
 import truffleContract from "truffle-contract";
 import SimpleStorageContract from "../../../jsonLoader";
-// import SimpleStorageContract from "../../../contracts/SimpleStrong.json";
-// const changeDetail = (title, content) => ({
-//   type: constants.CHANGE_DETAIL,
-//   title,
-//   content
-// });
 
 const authWeb3 = (web3, accounts, contract) => ({
   type: constants.AUTH_WEB3,
@@ -19,19 +13,20 @@ const authWeb3 = (web3, accounts, contract) => ({
 
 export const authWeb3Action = getWeb3 => {
   return async dispatch => {
-    console.log(SimpleStorageContract);
-    // try {
-    //   const web3 = await getWeb3;
-    //   // Use web3 to get the user's accounts.
-    //   const accounts = await web3.eth.getAccounts();
-    //   // Get the contract instance.
-    //   const Contract = truffleContract(SimpleStorageContract);
-    //   Contract.setProvider(web3.currentProvider);
-    //   const instance = await Contract.deployed();
-    //   dispatch(authWeb3(web3, accounts, instance));
-    // } catch (error) {
-    //   // Catch any errors for any of the above operations.
-    //   console.log(error);
-    // }
+    try {
+      const web3 = await getWeb3;
+      console.log(web3);
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+      console.log(web3);
+      // Get the contract instance.
+      const Contract = truffleContract(SimpleStorageContract);
+      Contract.setProvider(web3.currentProvider);
+      const instance = await Contract.deployed();
+      dispatch(authWeb3(web3, accounts, instance));
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      console.log(error);
+    }
   };
 };
