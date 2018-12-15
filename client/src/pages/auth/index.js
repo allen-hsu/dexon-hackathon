72d7fa280768c17b0dc5d13b1ea0304e760ed1de
Allen Hsu <allenhsu.taiwan@gmail.com>
2018-12-15 16:18:32 +0800

    add auth web3 component

--- client/src/pages/auth/index.js
+++ client/src/pages/auth/index.js
@@ -2,25 +2,29 @@ import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { actionCreators } from "./store";
-import { from } from "rxjs";
+import getWeb3 from "../../utils/getWeb3";
 class Auth extends PureComponent {
   render() {
-    return <div>沒有Web3</div>;
+    const { web3States } = this.props;
+    if (!web3States) {
+      return <div>有Web3</div>;
+    } else {
+      return <div>沒有Web3 loding</div>;
+    }
   }
 
   componentDidMount() {
-    this.props.getDetail(this.props.match.params.id);
+    this.props.authWeb3(getWeb3());
   }
 }
 
 const mapStateToProps = state => ({
-  title: state.getIn(["detail", "title"]),
-  content: state.getIn(["detail", "content"])
+  web3States: state.getIn(["auth", "web3"])
 });
 
 const mapDispathToProps = dispatch => ({
-  getDetail(id) {
-    dispatch(actionCreators.getDetail(id));
+  authWeb3(web3) {
+    dispatch(actionCreators.authWeb3Action(web3));
   }
 });
 
