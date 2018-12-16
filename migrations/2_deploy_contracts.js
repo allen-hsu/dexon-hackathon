6c739e7ab48adb0070b5e0cab1cf4a29c6d7745a
Allen Hsu <allenhsu.taiwan@gmail.com>
2018-12-16 15:26:11 +0800

    部署合約

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var Book = artifacts.require("./Book.sol");
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(Book, 10, 100);
+  deployer.deploy(Book, 10, 1000);
 };
