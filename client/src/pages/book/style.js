20b8b5a2b641c58496188c84a4488af692fd5cbe
Allen Hsu <allenhsu.taiwan@gmail.com>
2018-12-16 09:06:48 +0800

    加入排行榜的部分

--- client/src/pages/book/style.js
+++ client/src/pages/book/style.js
@@ -5,6 +5,21 @@ export const BookWrapper = styled.div`
   margin: 30px auto;
 `;
 
+export const BookLeft = styled.div`
+  position: relative;
+  margin: 30px auto;
+`;
+
+export const BookRight = styled.div`
+  position: relative;
+  margin: 30px auto;
+`;
+
+export const BookCenter = styled.div`
+  position: relative;
+  margin: 30px auto;
+`;
+
 export const PageWrapper = styled.div`
   position: relative;
   margin: 30px auto;
@@ -38,59 +53,44 @@ export const PageBox = styled.div`
   background-size: contain;
 `;
 
-
 export const PageItem = styled.div`
-    cursor: pointer;
+  cursor: pointer;
+  position: absolute;
+  color: black;
+  width: 100 %;
+  height: 100 %;
+  webkit-transition: 1.5 s - webkit - transform;
+  transition: 1.5 s transform;
+  -webkit-transform-style: preserve - 3 d;
+  transform-style: preserve - 3 d;
+  -webkit-transform-origin: left center;
+  -ms-transform-origin: left center;
+  transform-origin: left center;
+  background-color: red;
+  .front,
+  .back {
     position: absolute;
-    color: black;
-    width: 100 % ;
-    height: 100 % ;
-    webkit - transition: 1.5 s - webkit - transform;
-    transition: 1.5 s transform; -
-    webkit - transform - style: preserve - 3 d;
-    transform - style: preserve - 3 d; -
-    webkit - transform - origin: left center; -
-    ms - transform - origin: left center;
-    transform - origin: left center;
-    background-color: red;
-    .front,
-    .back {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-        -webkit-backface-visibility: hidden;
-        backface-visibility: hidden;
-    }
+    width: 100%;
+    height: 100%;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
 
-    .back {
-        -webkit-transform: rotateY(180deg);
-        transform: rotateY(180deg);
-    }
+  .back {
+    -webkit-transform: rotateY(180deg);
+    transform: rotateY(180deg);
+  }
 `;
 
-// export const Header = styled.div`
-//   margin: 50px 0 20px 0;
-//   line-height: 44px;
-//   font-size: 34px;
-//   color: #333;
-//   font-weight: bold;
-// `;
+export const ArticleWrapper = styled.div`
+  margin: 0px auto;
+`;
 
-// export const Content = styled.div`
-//   color: #2f2f2f;
-//   img {
-//     width: 100%;
-//   }
-//   p {
-//     margin: 25px 0;
-//     color: #222;
-//     font-size: 16px;
-//     line-height: 30px;
-//   }
-//   b {
-//     font-weight: bold;
-//   }
-// `;
+export const ArticleListWrapper = styled.div`
+  overflow: hidden;
+  width: 1280px;
+  margin: 0 auto;
+`;
